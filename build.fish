1751feb701638e6660e5e4d479c9f6f16af9a457
Ryan White <ryanwhite04@gmail.com>
2024-02-13 00:58:09 +0800

    Refactor

--- build.fish
+++ build.fish
@@ -1,25 +1,10 @@
 emsdk_setup
 emsdk activate latest
-
-# Check if the build directory exists and remove it
-if test -d dist
-    rm -r dist
-end
-mkdir dist
-
-# Check if the build directory exists and remove it
-if test -d build
-    sudo rm -r build
-end
-mkdir build
-
-# Create the build directory and navigate into it
-cd build
-
-# Configure the project with Emscripten's toolchain
+# empty the dist folder
+if test -d dist; rm -r dist; end; mkdir dist
+# empty the build folder
+if test -d build; sudo rm -r build; end; mkdir build
+pushd build
 emcmake cmake ..
-
-# Build the project
 emmake make
-# Go back to the wasm directory
-cd ..
+popd
