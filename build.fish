8f4da40f54e52c86da7bfdc2072973438890015c
Ryan White <ryanwhite04@gmail.com>
2024-02-26 13:08:17 +0800

    Build mujoco_wasm without lodepng

--- build.fish
+++ build.fish
@@ -1,10 +1,8 @@
-emsdk_setup
-emsdk activate latest
+emon
 # empty the dist folder
-if test -d dist; rm -r dist; end; mkdir dist
+# if test -d dist; rm -r dist; end; mkdir dist
 # empty the build folder
-if test -d build; sudo rm -r build; end; mkdir build
-pushd build
-emcmake cmake ..
-emmake make
-popd
+# if test -d build; rm -rf build; end; mkdir build
+# set -x EMCC_FORCE_STDLIBS 1
+emcmake cmake -B build --fresh
+emmake make -C build
